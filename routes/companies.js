e9fe45faecaf6bdbcd849dd630f58b317cf70528
rafael <rafaelpaula15@hotmail.com>
2021-10-28 18:50:57 -0400

    adding files

---
+++ routes/companies.js
@@ -0,0 +1,159 @@
+"use strict";
+
+/** Routes for companies. */
+
+const jsonschema = require("jsonschema");
+const express = require("express");
+
+const { BadRequestError } = require("../expressError");
+const { ensureAdmin } = require("../middleware/auth");
+// had to export ensureIsadmin to make sure the user has the right to create, update or delete 
+// const { ensureLoggedIn } = require("../middleware/auth");
+const Company = require("../models/company");
+
+const companyNewSchema = require("../schemas/companyNew.json");
+const companyUpdateSchema = require("../schemas/companyUpdate.json");
+
+const router = new express.Router();
+
+
+/** POST / { company } =>  { company }
+ *
+ * company should be { handle, name, description, numEmployees, logoUrl }
+ *
+ * Returns { handle, name, description, numEmployees, logoUrl }
+ *
+ * Authorization required: login
+ */
+//now user needs to be admin
+router.post("/", ensureAdmin, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, companyNewSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const company = await Company.create(req.body);
+    return res.status(201).json({ company });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /  =>
+ *   { companies: [ { handle, name, description, numEmployees, logoUrl }, ...] }
+ *
+ * Can filter on provided search filters:
+ * - minEmployees
+ * - maxEmployees
+ * - nameLike (will find case-insensitive, partial matches)
+ *
+ * Authorization required: none
+ */
+
+router.get("/", async function (req, res, next) {
+  try {
+    const companies = await Company.findAll();
+    return res.json({ companies });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /[handle]  =>  { company }
+ *
+ *  Company is { handle, name, description, numEmployees, logoUrl, jobs }
+ *   where jobs is [{ id, title, salary, equity }, ...]
+ *
+ * Authorization required: none
+ */
+
+router.get("/:handle", async function (req, res, next) {
+  try {
+    const company = await Company.get(req.params.handle);
+    return res.json({ company });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+// this router can be use to get companies by name 
+// this needs to be adjusted too on models folder 
+router.get("/single-company/:name", async function (req, res, next) {
+  try {
+    const company = await Company.findOne(req.params.name);
+    return res.json({ company });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+//find companies with minEmployees
+router.get("/min-employees/:num", async function (req, res, next) {
+  try {
+    const company = await Company.minEmployees(req.params.num);
+    return res.json({ company });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+//find companies with maxEmployees
+router.get("/max-employees/:num", async function (req, res, next) {
+  try {
+    const company = await Company.maxEmployees(req.params.num);
+    return res.json({ company });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+
+
+/** PATCH /[handle] { fld1, fld2, ... } => { company }
+ *
+ * Patches company data.
+ *
+ * fields can be: { name, description, numEmployees, logo_url }
+ *
+ * Returns { handle, name, description, numEmployees, logo_url }
+ *
+ * Authorization required: login
+ */
+//now user needs to be admin
+router.patch("/:handle", ensureAdmin, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, companyUpdateSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const company = await Company.update(req.params.handle, req.body);
+    return res.json({ company });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** DELETE /[handle]  =>  { deleted: handle }
+ *
+ * Authorization: login 
+ */
+// now user needs to be admin
+
+router.delete("/:handle", ensureAdmin, async function (req, res, next) {
+  try {
+    await Company.remove(req.params.handle);
+    return res.json({ deleted: req.params.handle });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+module.exports = router;
